b072d892c50533ea508c04c79c805db0c3921c12
Barberousse93 <mobile.barberousse@gmail.com>
2023-11-20 08:40:56 +0100

    factorisation ListeVilles (3)

--- src/components/ListeVilles.jsx
+++ src/components/ListeVilles.jsx
@@ -9,45 +9,45 @@ import Select from '@mui/material/Select'
 import Skeleton from '@mui/material/Skeleton'
 import { store } from '../App'
 import { villeSelectionnee, addHistorique, villeRecherchee } from '../actions/ville.action'
-import useFetch from '../utils/Hooks/useFetch'
+// import useFetch from '../utils/Hooks/useFetch'
 import useListeVille from '../utils/Hooks/useListeVille'
 
 function ListeVilles() {
-  const { isVisible, setIsVisible, villeChoisie, handleChange } = useListeVille()
-  const [url, setUrl] = useState('')
-  const [liste, setListe] = useState([])
-
-  const { data, isLoading, error } = useFetch(url)
+  const { isLoading, error, liste, isVisible, setIsVisible, villeChoisie, handleChange } =
+    useListeVille()
+  // const [url, setUrl] = useState('')
+  // const [liste, setListe] = useState([])
+  // const { data, isLoading, error } = useFetch(url)
 
   const Ville = useSelector((state) => state.ville.villeRecherchee)
   const insee = useSelector((state) => state.ville.villeSelectionnee)
 
-  useEffect(() => {
-    if (Ville.length > 0) {
-      fetchCities()
-    }
-  }, [Ville])
+  // useEffect(() => {
+  //   if (Ville.length > 0) {
+  //     fetchCities()
+  //   }
+  // }, [Ville])
 
-  useEffect(() => {
-    if (data && data['cities']) {
-      const liste = data['cities']
+  // useEffect(() => {
+  //   if (data && data['cities']) {
+  //     const liste = data['cities']
 
-      if (liste.length === 1) {
-        store.dispatch(villeSelectionnee(liste[0].insee))
-        store.dispatch(addHistorique({ insee: liste[0].insee, name: liste[0].name }))
-        setIsVisible(false)
-      } else {
-        setListe(liste)
-      }
-    }
-  }, [data])
+  //     if (liste.length === 1) {
+  //       store.dispatch(villeSelectionnee(liste[0].insee))
+  //       store.dispatch(addHistorique({ insee: liste[0].insee, name: liste[0].name }))
+  //       setIsVisible(false)
+  //     } else {
+  //       setListe(liste)
+  //     }
+  //   }
+  // }, [data])
 
-  async function fetchCities() {
-    const urlBase = 'https://api.meteo-concept.com/api/'
-    const token2 = 'c90958e683691c5251a4ecc2aec3e22349c67d7f262f60ed04fce5741552d263'
-    const url = urlBase + 'location/cities/?token=' + token2 + '&search=' + Ville
-    setUrl(url)
-  }
+  // async function fetchCities() {
+  //   const urlBase = 'https://api.meteo-concept.com/api/'
+  //   const token2 = 'c90958e683691c5251a4ecc2aec3e22349c67d7f262f60ed04fce5741552d263'
+  //   const url = urlBase + 'location/cities/?token=' + token2 + '&search=' + Ville
+  //   setUrl(url)
+  // }
 
   if (error) {
     return <div>{error} </div>
--- src/utils/Hooks/useListeVille.js
+++ src/utils/Hooks/useListeVille.js
@@ -1,9 +1,13 @@
 import { useSelector } from 'react-redux'
 import { useState, useEffect } from 'react'
+import useFetch from '../../utils/Hooks/useFetch'
 
 export default function useListeVille() {
   const [isVisible, setIsVisible] = useState(true)
   const [villeChoisie, setVilleChoisie] = useState('')
+  const [url, setUrl] = useState('')
+  const [liste, setListe] = useState([])
+  const { data, isLoading, error } = useFetch(url)
 
   const Ville = useSelector((state) => state.ville.villeRecherchee)
 
@@ -19,7 +23,37 @@ export default function useListeVille() {
     setVilleChoisie(e.target.value)
   }
 
+  useEffect(() => {
+    if (Ville.length > 0) {
+      fetchCities()
+    }
+  }, [Ville])
+
+  useEffect(() => {
+    if (data && data['cities']) {
+      const liste = data['cities']
+
+      if (liste.length === 1) {
+        store.dispatch(villeSelectionnee(liste[0].insee))
+        store.dispatch(addHistorique({ insee: liste[0].insee, name: liste[0].name }))
+        setIsVisible(false)
+      } else {
+        setListe(liste)
+      }
+    }
+  }, [data])
+
+  async function fetchCities() {
+    const urlBase = 'https://api.meteo-concept.com/api/'
+    const token2 = 'c90958e683691c5251a4ecc2aec3e22349c67d7f262f60ed04fce5741552d263'
+    const url = urlBase + 'location/cities/?token=' + token2 + '&search=' + Ville
+    setUrl(url)
+  }
+
   return {
+    isLoading,
+    error,
+    liste,
     isVisible,
     setIsVisible,
     villeChoisie,
